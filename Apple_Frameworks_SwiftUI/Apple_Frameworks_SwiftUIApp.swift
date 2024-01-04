881a16db5763ef896b91b98eba74e957570ddbd8
FaizaNawaz75 <Faizanawaz755@gmail.com>
2024-01-04 21:49:45 +0400

    Initial Commit

---
+++ Apple_Frameworks_SwiftUI/Apple_Frameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_SwiftUIApp.swift
+//  Apple_Frameworks_SwiftUI
+//
+//  Created by Faiza Nawaz on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
